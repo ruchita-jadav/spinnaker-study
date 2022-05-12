f4e20b210b14cd4f003d71d9dfb887637aa68df1
rjadav <rjadav@salesforce.com>
2022-05-12 11:52:10 +0530

    terraform changes

--- terraform/app.tf
+++ terraform/app.tf
@@ -26,7 +26,7 @@ terraform {
    backend "s3" {
  # Replace this with your bucket name!
    bucket         = "ot-infra-state"
-   key            = "global/s3/terraform-test.tfstate"
+   key            = "global/s3/terraform-interesting-unit.tfstate"
    region         = "us-east-1"
    }
 }
